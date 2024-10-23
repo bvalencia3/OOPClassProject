3047b6ecf132f0680b05ed1e2a1447d97923f66d
Saul Gonzalez <70164571+Saulo95@users.noreply.github.com>
2024-10-22 20:22:15 -0600

    Update Person.java

--- Person.java
+++ Person.java
@@ -13,63 +13,33 @@ public abstract class Person {
      * @param name1 first name
      * @param name2 last name
      */
-    public Person(String name1, String name2) {
-        this.firstName = name1;
-        this.lastName = name2;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     // getters and setters
-    public void setFirstName(String name1) {
-        this.firstName = name1;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getFirstName() {
         return this.firstName;
     }
 
-    public void setLastName(String name2) {
-        this.lastName = name2;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getLastName() {
         return this.lastName;
     }
+    
+
+    // Method to display person's full name
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
 
-/**
- * A subclass of Person, representing a Customer
- * @author Gian Hernandez
- */
-public class Customer extends Person {
-    private int id;
 
-    /**
-     * A default constructor for the Customer class
-     */
-    public Customer() {
-        super("","");
-        this.id = -1;
-    }
-
-    /**
-     * A constructor for the Customer class which takes 3 parameters/
-     * @param name1 value to be assigned to firstName
-     * @param name2 value to be assigned to lastName
-     * @param id value to be assigned to id
-     */
-    public Customer(String name1, String name2, int id) {
-        super(name1,name2);
-        this.id = id;
-    }
-
-    // getter and setter
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    //public boolean isCustomer() {}
-}
